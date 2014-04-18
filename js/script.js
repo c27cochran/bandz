d5fa21f14a0b78e85ba33052ad20d559a702e79a
Carter Cochran <carter.cochran@gmail.com>
2014-04-18 01:53:33 -0500

    reformatted

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-(function(){
+(function () {
     var poller = new window.massrel.Poller({
-      limit: 5, // slice to 5 with processData()
-      frequency: 15 // update every 15 seconds
-    }, function(){
+        limit: 5, // slice to 5 with processData()
+        frequency: 15 // update every 15 seconds
+    }, function () {
         var bandArray = this.processData();
 
-        // return a number with the correct comma delimited formatting
-        function numberWithCommas(x) {
-            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        }
-
-        // run some fun CSS3 animation magic!
-        setTimeout(function(){
-            $('.fade').addClass('animation rotateLeft');
-        }, 15000);
-
-        $('.bands').fadeOut(function(){
+        $('.bands').fadeOut(function () {
             $(this).children().remove();
-            for (i = 0; i < bandArray.length; i++){
+            for (i = 0; i < bandArray.length; i++) {
                 $('.bands').append('<li class="fade"><span>' + bandArray[i].name + '</span> <span class="right"><span class="red">' + numberWithCommas(bandArray[i].count) + '</span> <small>Mentions</small></span></li>');
             }
         }).fadeIn();
-      });
+    });
 
-    poller.start(); 
+    // return a number with the correct comma delimited formatting
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    // run some fun CSS3 animation magic!
+    setTimeout(function () {
+        $('.fade').addClass('animation rotateLeft');
+    }, 15000);
+
+    poller.start();
 }())
